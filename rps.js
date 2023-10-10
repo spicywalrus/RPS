c1ec1d4acab942ddfcd0fd7d8995dd649b23b28b
Alex Zeitler <alexgzeitler@gmail.com>
2023-10-09 17:11:48 -0700

    Add helper functions and define game logic

--- rps.js
+++ rps.js
@@ -1,24 +1,60 @@
 
 //Functions
+function promptPlayer(playerChoice){
+    let lowerCaseChoice = toLowerCase(playerChoice);//take prompt and make string into all lowercase.
+    playerSelection = lowerCaseChoice; //assign lowercase string to player selection
+}
 function playRound(playerSelection, computerSelection) {
-    // your code here!
+    if (playerSelection === computerSelection){
+        console.log("Tie. Redo.");
+        //prompt player again
+    }
+    else if (playerSelection === "rock" && computerSelection === "paper"){
+        console.log("You lose!");
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors"){
+        console.log("You win!");
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper"){
+        console.log("You win!");
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock"){
+        console.log("You lose!");
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock"){
+        console.log("You win!");
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors"){ //probably much more efficient way to do this
+        console.log("You lose!");
+    }
   }
-function getComputerChoice(){
-    let rpsChoice = "rock";
-    let rpsSelector = getRandomArbitrary(1,3);
-    switch (rpsSelector){
+function getComputerChoice(){ //does this return a string?
+    let rpsChoice = "poopie"; //initialized
+    let rpsSelector = getRandomArbitrary(1,3); //select random # between 1 and 3
+    switch (rpsSelector){ //assign and return selected string
         case '1':
             rpsChoice = "rock";
         case '2':
             rpsChoice = "paper";
         case '3':
             rpsChoice = "scissors";
+        default:
+            "default rock";
+        break;
     }
+    console.log(rpsChoice);
+    return rpsChoice; //should return switch statement result
+    
 }
-function getRandomArbitrary(min, max) {
+function getRandomArbitrary(min, max) { //Selects random number to help assign symbol choice
     return Math.random() * (max - min) + min;
+}
+function game(){
+    //runs whole game
 }
 //Variables
   const playerSelection = "rock";
   const computerSelection = getComputerChoice();
   console.log(playRound(playerSelection, computerSelection));
+
+//Log
